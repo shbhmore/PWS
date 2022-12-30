f088d355dc186902c9cfbf7c16b1c25f0bd2b5e5
shbhmore <mr.smore77@gmail.com>
2022-12-31 00:13:24 +0530

    Started developing Registration and Login page

--- PWS/src/app/app.module.ts
+++ PWS/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../UI/header/header.component';
+import { MenubarComponent } from 'src/UI/menubar/menubar.component';
+import { MainComponent } from 'src/UI/main/main.component';
+import { FooterComponent } from 'src/UI/footer/footer.component';
+import { EmployeeComponent } from '../components/employee/employee.component';
+import { SkillsComponent } from '../components/skills/skills.component';
+import { OrganizationComponent } from '../components/organization/organization.component';
+import { ExperienceComponent } from '../components/experience/experience.component';
+import { PersonComponent } from '../components/person/person.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenubarComponent,
+    MainComponent,
+    FooterComponent,
+    EmployeeComponent,
+    SkillsComponent,
+    OrganizationComponent,
+    ExperienceComponent,
+    PersonComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
