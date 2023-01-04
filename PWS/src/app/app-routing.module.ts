2835e0a73a6459b1a568f21966ab41aad1e3ebeb
shbhmore <mr.smore77@gmail.com>
2023-01-04 23:06:57 +0530

    Designed Register and Login with different CSS style, Added more content on register screen, updated css for whole main home screen

--- PWS/src/app/app-routing.module.ts
+++ PWS/src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'login',component:LoginComponent},
-  {path:'register',component:RegisterComponent}
+  {path:'',component:HomeComponent,
+    children:[
+      {path:'login',component:LoginComponent},
+      {path:'register',component:RegisterComponent}
+    ]}
 ];
 
 @NgModule({
